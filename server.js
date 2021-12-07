6df1d5873c99b459e2f1a9c7f68be5efccfa4beb
Omer Tulumen <72311799+OmerTulumen@users.noreply.github.com>
2021-12-07 22:28:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-require('dotenv').config();
+if(process.env.NODE_ENV) {
+  require('dotenv').config({
+   path: `${__dirname}/.env.${process.env.NODE_ENV}`,
+  });
+} else {
+  require('dotenv').config(); 
+}
 const app = require("./app");
 const port = process.env.PORT || 3000;
-
+console.log(port);
 app.listen(port);
 console.log("Server running (3000)");
